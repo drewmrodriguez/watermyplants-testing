686be143298015a89bd2fdd77a199dd0dadc6a7a
drewmrodriguez <drewmrodriguez@gmail.com>
2021-08-29 15:52:53 -0500

    working

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const usersRouter = require("./users/users-router");
-const plantsRouter = require("./plants/plants-router");
+const usersRouter = require("./routers/users-router");
+const plantsRouter = require("./routers/plants-router");
 
 const server = express();
 server.use(express.json());
@@ -13,12 +13,12 @@ server.use(cors());
 server.use("/api/users", usersRouter);
 server.use("/api/plants", plantsRouter);
 
-//SANITY CHECK ENDPOINT
-// server.get("/", (req, res, next)=>{
-//     res.json({
-//         message: "API Up"
-//     })
-// })
+// SANITY CHECK ENDPOINT
+server.get("/", (req, res, next)=>{
+    res.json({
+        message: "API Up"
+    })
+})
 
 //Global Error Handling
 server.use((err, req, res, next) => {
